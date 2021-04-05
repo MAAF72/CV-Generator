2e8df777d6be11ad7bcefe66e18250de4dac9ae1
hexid <60289816+heexid@users.noreply.github.com>
2021-04-05 22:04:57 +0700

    update form-bahasa dropdown when click button addmore

--- application/web/static/js/input.js
+++ application/web/static/js/input.js
@@ -183,20 +183,28 @@ $(function() {
     function add_bahasa() {
         const html = `
         <div id="content-bahasa">
-            <div class="div-content-row">
-                <input type="text" class="form-control" name="nama" aria-describedby="nama" placeholder="Bahasa">
-                <div style="width: 16px;"></div>
-                <input type="text" class="form-control" id="level" name="level" aria-describedby="level" placeholder="Level">
-            </div>
-            
-            <div class="row justify-content-end">
-                <button type="button" id="delete-bahasa" class="btn btn-outline-danger justify-content-end"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                </svg>
-            </button>
-            </div>
-            <hr>
-        </div>
+                                <div class="div-content-row">
+                                    <input type="text" class="form-control" name="nama" aria-describedby="nama" placeholder="Bahasa">
+                                    <div style="width: 16px;"></div>
+                                    <select class="custom-select" id="level" name="level">
+                                        <option hidden value="">Choose Level</option>
+                                        <option value="Elementary">Elementary</option>
+                                        <option value="Professional">Professional</option>
+                                        <option value="Native">Native</option>
+                                    </select>
+                                    <!--
+                                        <input type="text" class="form-control" id="level" name="level" aria-describedby="level" placeholder="Level">
+                                        -->
+                                </div>
+                                <div class="row justify-content-end">
+                                    <button type="button" id="delete-bahasa" class="btn btn-outline-danger justify-content-end">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                                <hr>
+                            </div>
         `
         $('#form-bahasa').append(html);
     }
