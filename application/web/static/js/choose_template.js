937e2ba4abd7f71c46a3f33126f4f639afcf5239
hexid <60289816+heexid@users.noreply.github.com>
2021-04-18 14:42:17 +0700

    complete choosetemplate

--- application/web/static/js/choose_template.js
+++ application/web/static/js/choose_template.js
@@ -2,10 +2,22 @@ $(function() {
     const path_name = window.location.pathname.split('/')
     const unique_code = path_name[2]
 
+    let template_id = null
     console.log(`Unique Code = ${unique_code}`)
 
-    $('button').click(() => {
+    $('#download-cv').click(() => {
         console.log(`Unique Code = ${unique_code}`)
+        console.log(template_id);
         //do operation here to update cv's template with selected template then proceed to download page
     })
+
+    //function select one template
+    $('.row .radio').click(function(){
+        $(this).parent().find('.radio').removeClass('selected');
+        $(this).addClass('selected');
+        template_id = $(this).attr('data-value');
+    });
+
+    
+
 })
